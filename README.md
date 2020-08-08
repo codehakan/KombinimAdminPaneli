d973227d1509bfca80e59e4e9853121f0385b001
HAKANBEY <code.hakan@gmail.com>
2020-08-08 10:52:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# KombinimAdminPaneli
+ 
---
+++ app/src/main/java/com/hakanbey/kombinimadminpaneli/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hakanbey.kombinimadminpaneli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Giriş başarıyla sağlandı.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MainActivity
+    }
+}
